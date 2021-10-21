bf07f8be3724712b8ec2f2535fb05840d0ee2ade
Brian Clow <brianclow@u.boisestate.edu>
2021-10-21 15:57:34 -0600

    Add initial app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,101 @@
-import { Component, OnInit } from '@angular/core'
-
-import { DataService, DataItem } from '../shared/data.service'
+import { Component } from '@angular/core'
+import { EventData, View } from '@nativescript/core';
+import * as SocialShare from "@nativescript/social-share";
+import * as fs from '@nativescript/core/file-system'
+import { ImageSource } from '@nativescript/core/image-source';
 
 @Component({
   selector: 'Home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
-  items: Array<DataItem>
+export class HomeComponent {
 
-  constructor(private _itemService: DataService) {}
 
-  ngOnInit(): void {
-    this.items = this._itemService.getItems()
+  saveScore(args: EventData) {
+    console.log("====================================");
+    console.log("Save");
+
+    // const view = args.object as View;
+    // const targetView = view.page.getViewById('score') as View;
+
+    // // let documents = fs.knownFolders.ios.downloads();
+    // // console.log("documents", documents);
+
+    // // let path = fs.path.join(documents.path, "pic.png");
+    // // console.log("path", path);
+
+    // const screenShot = this.getImage(targetView);
+    // // screenShot.saveToFile(path, "jpg");
+
+
+
+
+
+    // // let path      = fs.path.join(fs.knownFolders.documents().path, "Test.png");    
+    // // let src       = ImageSource.fromNativeSource(img);
+    // // let saved     = src.saveToFile(path, "png");
+
+
+    // // var knownPath = fs.knownFolders.currentApp();
+    // // var folderPath = fs.path.join(knownPath.path, "CosmosDataBank");
+
+    // // var folder = fs.Folder.fromPath(folderPath);
+    // // var path = fs.path.join(folderPath, "Test.png");
+
+    // // var saved = screenShot.saveToFile(path, "png");
+
+    // // let documents = fs.knownFolders.documents();
+    // // let path = fs.path.join(documents.path, "pic.png");
+    // // ImageSource fromDataSync(screenShot)
+    // //     .then(imageSource => {
+    // //          imageSource.saveToFile(path, "png");
+    // //      });
+
+
+
+    // console.log("screenshot", screenShot);
+
   }
+
+
+  shareScore(args: EventData) {
+    console.log("====================================");
+    console.log("Share");
+
+    const view = args.object as View;
+
+    const targetView = view.page.getViewById('score') as View;
+
+    const screenShot = this.getImage(targetView);
+
+    // let image = ImageSource.fromFile("~/path/to/myImage.jpg");
+    let image = screenShot;
+    SocialShare.shareImage(image);
+
+
+  }
+
+  getImage(view: View) {
+    if (view.ios) {
+      //ios logic
+
+      UIGraphicsBeginImageContextWithOptions(view.ios.frame.size, false, 0);
+
+
+      view.ios.drawViewHierarchyInRectAfterScreenUpdates(CGRectMake(0, 0, view.ios.frame.size.width,
+        view.ios.frame.size.height), true);
+      var imageFromCurrentImageContext = UIGraphicsGetImageFromCurrentImageContext();
+      UIGraphicsEndImageContext();
+      return ImageSource.fromDataSync(UIImagePNGRepresentation(imageFromCurrentImageContext));
+
+
+    } else if (view.android) {
+      // android logic
+
+    }
+    return undefined;
+
+  }
+
+
 }
